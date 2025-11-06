c6f77c53c4eca53d4bfb7f3e4435d5257e0a565e
kmschoenecker <kschoenecker@wisc.edu>
2025-11-06 14:56:37 -0600

    r script

---
+++ Week10_HW.R
@@ -0,0 +1,21 @@
+##### Week 10 Homework ----
+# Kristine Schoenecker, Nathan Lin, Elizabeth Braatz
+
+#### Objective 1 ----
+
+#### Part A 
+# Using either your own data or data that you find in an online database,
+#find two continuous variables that might reasonably be hypothesized to have a 
+#causal association (i.e., one variable is clearly the response, Y, and the 
+#other the predictor, X)
+#and have sufficient numbers of paired observations (> 30)
+
+#### Part B
+#### Part C
+#### Part D
+
+#### Objective 2 ----
+
+#### Part A
+#### Part B
+#### Part C
