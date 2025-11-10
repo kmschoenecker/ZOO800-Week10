6a36868bee5c518f916bf19def8a2a52763e15a1
kmschoenecker <kschoenecker@wisc.edu>
2025-11-09 22:36:48 -0600

    got stuck on the predicting part for obj 1, skipped to obj 2 instead. Got up to part c

--- Week10_HW.R
+++ Week10_HW.R
@@ -48,17 +48,80 @@ bee_data$diff_weight <- bee_data$weight_out_of_nest_g - bee_data$weight_of_bee_a
 
 model_1 <- lm(diff_weight ~ total_distance_m, data = bee_data)
 
-#scatterplot of this relationship
+#scatterplot of this relationship, and we can add the model regression to this plot
 
 scatter_1 <- ggplot(data = bee_data, mapping = aes(x = total_distance_m, y = diff_weight)) +
-  geom_point()
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red")
+
 scatter_1
 
 #### Part C
-#### Part D
+
+#use the plot() function to look at the various assumptions and see how well our model holds up
+
+plot(model_1)
+#first plot generated is residuals vs fitted
+#we see a couple of outliers in this chart, not great.
+#Also, most of our data is clustered near the left side of the graph
+
+#the second plot generated is a QQ plot
+#The middle of the plot seems to follow a straight line, which is great
+#But neither the high or low end of the QQ plot follow that straight line, not great
+#This means, I believe, that we do not have a normal distribution going on at the ends of our relationship
+
+#the third plot is scale-location
+#this one again shows some outliers, the red line is sort of horizontal, so I am guessing our
+#assumption of heteroscedasicity is not too terribly violated?
+
+#the fourth plot is the residuals vs leverage
+#none of the points appear to be outside the dashed line that would indicate that
+#they would highly influence the data? Is that the correct interpretation?
+
+#### Part D Generate predictions and prediction intervals for the median and 95th percentile of X
+
+#we can use the summary function to get the median
+
+summary(bee_data$total_distance_m)
+#median distance flown was 101.76 m
+#to get the 95th quantile
+quantile(bee_data$total_distance_m, probs = 0.95, na.rm = TRUE)
+#the 95th quantile is 4080.576, whoa. That is really different from the median
+
+#use the predict() function to generate predictions
+#I am actually a little confused on how to do this
+#in the examples I was looking at online, it seems like you end up putting the model into the predict
+#function and then the data you have as new_data? 
+#but if we enter the x, y values from the data, how do we get predictions?
+
 
 #### Objective 2 ----
 
-#### Part A
-#### Part B
+#### Part A Generate linear regression data where the error in the response variable Y is not quite normally
+#distributed (but still unimodal). A lognormal or negative binomial distribution should work. No
+#error in X this time. 100 X, Y pairs should be good.
+
+#by no error in X, does that mean generate from a normal distribution?
+
+X <- rnorm(100)
+Y <- rlnorm(100) #to generate log normal values
+
+#### Part B Fit a linear regression to this data
+
+model_2 <- lm(Y ~ X)
+summary(model_2)
+
+plot(model_2)
+
 #### Part C
+
+intercept <- coef(model_2)[1]
+slope <- coef(model_2)[2] #pulls out and saves the slope from our model
+
+#what do you mean, true value for the slope and intercept?
+#like, just if we had plotted them as a scatterplot the slope and intercept?
+
+#### Part D
+#### Part E
+#### Part F
+#### Part G
