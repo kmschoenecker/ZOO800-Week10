e215f5cb83eaf39f187d08dec1d904ddc28919e8
kmschoenecker <kschoenecker@wisc.edu>
2025-11-09 21:36:35 -0600

    adding some info on the paper we chose to use for data

--- Week10_HW.R
+++ Week10_HW.R
@@ -7,8 +7,11 @@
 # Using either your own data or data that you find in an online database,
 #find two continuous variables that might reasonably be hypothesized to have a 
 #causal association (i.e., one variable is clearly the response, Y, and the 
-#other the predictor, X)
-#and have sufficient numbers of paired observations (> 30)
+#other the predictor, X) and have sufficient numbers of paired observations (> 30)
+
+#For our homework, we found this paper from Kenna et al (2021) in Functional Ecology
+#title: Thermal flight performance reveals impact of warming on bumblebee foraging potential
+#relevant since Elizabeth and I both work on bumble bees
 
 #### Part B
 #### Part C
